3e18d5bc033d6f22b25b59acff34842758d5bc42
Chris Kim <kimjm010@icloud.com>
2022-09-22 17:34:56 +0900

    [StreamingPlayer - Add, Edit]
    - StreamingKit 프레임워크 생성 및 리팩토링
    - 재생, 일시정지 기능 추가
    - 음소거, 소리 키는 기능 추가
    - 앞,뒤 10초 이동할 수 있는 기능 추가

---
+++ StreamingKit/StreamingVideoPlayer.swift
@@ -0,0 +1,86 @@
+//
+//  File.swift
+//  StreamingKit
+//
+//  Created by Chris Kim on 9/22/22.
+//
+
+import AVKit
+import AVFoundation
+
+
+public class StreamingVideoPlayer {
+    
+    public init() { }
+    
+    // MARK: - Vars
+    
+    private let playerViewController = AVPlayerViewController()
+    
+    private let avPlayer = AVPlayer()
+    
+    private lazy var playerView: UIView = {
+        let view = playerViewController.view!
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    
+    // MARK: - Public Interface
+    
+    /// superView에 playerViewf를 추가하기 위한 메소드
+    public func add(to view: UIView) {
+        view.addSubview(playerView)
+        NSLayoutConstraint.activate([
+            playerView.topAnchor.constraint(equalTo: view.topAnchor),
+            playerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            playerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            playerView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    // MARK: - Control Play
+    
+    public func play(url: URL) {
+        let asset = AVAsset(url: url)
+        let playerItem = AVPlayerItem(asset: asset)
+        avPlayer.replaceCurrentItem(with: playerItem)
+        playerViewController.player = avPlayer
+        playerViewController.player?.play()
+    }
+    
+    
+    public func pause() {
+        avPlayer.pause()
+    }
+    
+    
+    // MARK: - Move to Forward and Backward
+    
+    public func moveToBackward() {
+        let currentTime = CMTimeGetSeconds(avPlayer.currentTime())
+        let newTime = currentTime - 10
+        let setTime: CMTime = CMTimeMake(value: Int64(newTime * 1000 as Float64), timescale: 1000)
+        avPlayer.seek(to: setTime, toleranceBefore: CMTime.zero, toleranceAfter: CMTime.zero)
+    }
+    
+    
+    public func moveToForward() {
+        let currentTime = CMTimeGetSeconds(avPlayer.currentTime())
+        let newTime = currentTime + 10
+        let setTime: CMTime = CMTimeMake(value: Int64(newTime * 1000 as Float64), timescale: 1000)
+        avPlayer.seek(to: setTime, toleranceBefore: CMTime.zero, toleranceAfter: CMTime.zero)
+    }
+    
+    
+    // MARK: - Control Volume
+    
+    public func turnOnVolume() {
+        avPlayer.volume = 1.0
+    }
+    
+    
+    public func muteVolume() {
+        avPlayer.volume = 0.0
+    }
+}
