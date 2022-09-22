dc2637e7935f65a33cb168e2170aa789c55fa663
Chris Kim <kimjm010@icloud.com>
2022-09-23 00:23:53 +0900

    [StreamingPlayer - Add]
    - MP4플레이어 기능을 위한 비디오 추가

--- ChStreamingPlayer/StreamingViewController.swift
+++ ChStreamingPlayer/StreamingViewController.swift
@@ -25,10 +25,10 @@ class StreamingViewController: UIViewController {
     
     @IBAction func playButtonTapped() {
         
-        let urlStr = "https://livecloud.pstatic.net/navertv/lip2_kr/cnmss0068/UiOA18Ees6RVFEK2gWBdqh_3Z1isVvucV4lBZA4DnM96WrRiL8MF4j6ouvDO_g3k4OWk2r_1sxE0boeN/hdntl=exp=1663889041~acl=*%2FUiOA18Ees6RVFEK2gWBdqh_3Z1isVvucV4lBZA4DnM96WrRiL8MF4j6ouvDO_g3k4OWk2r_1sxE0boeN%2F*~data=hdntl~hmac=85b62c82e6ae7d02ccab57b4ffaa46e5ce8850f09ab3f89ba8aec7c8e551e543/chunklist_720.m3u8"
+        let urlStr = "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8"
         
         guard let url = URL(string: urlStr) else {
-            print(#function, #file, #line, "Error Parsing Url")
+            print(#function, #file, #line, "Error Occurred when Parsing Url")
             return
         }
         
